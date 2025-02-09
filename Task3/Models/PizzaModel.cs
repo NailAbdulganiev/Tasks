e8989f01aacafea6aff78c10d52f6ddd25317ef2
NailAbdulganiev <nail_abdulganiev@mail.ru>
2025-02-09 23:13:52 +0400

    Создание репозитория для списка пицц, на основании интерфейса IPizzaRepository. Создании списка объектов на основании нашей модели PizzaModel. Метод GetAllPizza() возвращает список всех пицц.

--- Task3/Models/PizzaModel.cs
+++ Task3/Models/PizzaModel.cs
@@ -11,7 +11,7 @@ namespace Task3.Models
         public string ImageUrl { get; set; } = string.Empty;
         public Dictionary<int, int> SizeToPrice { get; set; } = new Dictionary<int, int>();
         public Dictionary<int, int> SizeToWeight { get; set; } = new Dictionary<int, int>();
-        public string[] DoughTypes { get; set; } = new string[] { "Традиционное", "Толстое" };
+        public string[] DoughTypes { get; set; } = ["Традиционное", "Толстое"];
 
     }
 }
