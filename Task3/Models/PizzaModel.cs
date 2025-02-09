5986e88ed01093e1d60da5c7e5bc738f5a0d18ea
NailAbdulganiev <nail_abdulganiev@mail.ru>
2025-02-09 21:55:16 +0400

    Создал модель PizzaModel для нашего проекта.

---
+++ Task3/Models/PizzaModel.cs
@@ -0,0 +1,17 @@
+﻿using System.Diagnostics.Contracts;
+using System.Reflection;
+
+namespace Task3.Models
+{
+    public class PizzaModel
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public string Description { get; set; } = string.Empty;
+        public string ImageUrl { get; set; } = string.Empty;
+        public Dictionary<int, int> SizeToPrice { get; set; } = new Dictionary<int, int>();
+        public Dictionary<int, int> SizeToWeight { get; set; } = new Dictionary<int, int>();
+        public string[] DoughTypes { get; set; } = new string[] { "Традиционное", "Толстое" };
+
+    }
+}
