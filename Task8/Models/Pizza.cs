8fce2ab05e21be953facdec3acea572f5c8965b3
NailAbdulganiev <nail_abdulganiev@mail.ru>
2025-02-25 23:34:03 +0400

    Скопировал проект, создал БД в MS SQL, заполнил таблицы. С помощью Scaffold и Entity выполнил подключение. Создались файлы PizzaSize.cs и Pizza.cs

---
+++ Task8/Models/Pizza.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Task8.Models;
+
+public partial class Pizza
+{
+    public int Id { get; set; }
+
+    public string Name { get; set; } = null!;
+
+    public string Description { get; set; } = null!;
+
+    public string ImageUrl { get; set; } = null!;
+
+    public virtual ICollection<PizzaSize> PizzaSizes { get; set; } = new List<PizzaSize>();
+}
---
+++ Task8/Models/PizzaSize.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Task8.Models;
+
+public partial class PizzaSize
+{
+    public int Id { get; set; }
+
+    public int PizzaId { get; set; }
+
+    public int Size { get; set; }
+
+    public int Price { get; set; }
+
+    public int Weight { get; set; }
+
+    public virtual Pizza Pizza { get; set; } = null!;
+}
